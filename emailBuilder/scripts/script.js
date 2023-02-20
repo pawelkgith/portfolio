f24bbdb8f9046b3231632f2a44f29c9596fe5a4c
paweu2137 <99692169+paweu2137@users.noreply.github.com>
2023-02-20 21:58:30 +0100

    Add files via upload

---
+++ emailBuilder/scripts/script.js
@@ -0,0 +1,121 @@
+/* let txt = [] 
+txt = document.querySelectorAll("#i");
+
+document.querySelector(".btn").onclick = () => {
+    navigator.clipboard.writeText(txt[0].innerHTML + txt[1].innerHTML);
+} */
+
+const recipents = document.querySelectorAll("#recipents"),
+examples = document.querySelectorAll("#examples"),
+endings = document.querySelectorAll("#endings"),
+greetings = document.querySelectorAll("#greetings"),
+recipentsDiv = document.querySelector(".body__recipents"),
+examplesDiv = document.querySelector(".body__examples"),
+endingsDiv = document.querySelector(".body__endings"),
+greetingsDiv = document.querySelector(".body__greetings"),
+copyBtn = document.querySelector(".copy"),
+resetBtn = document.querySelector(".reset");
+
+resetBtn.addEventListener("click", () => {
+    window.location.reload();
+});
+
+for(let i=0; i<recipents.length; i++) {
+    recipents[i].addEventListener("click", () => {
+        let fs = 1;
+        let text = recipents[i].value;
+        recipents.forEach(el => el.remove());
+        let p = document.createElement("p");
+        p.setAttribute("class", "generatedText");
+        p.setAttribute("id", "recipentsText");
+        p.textContent = text;
+        recipentsDiv.append(p);
+        let recipentsInterval = setInterval(() => {
+            p.style.fontSize = `${fs}px`;
+            fs++;
+
+            if(fs === 24)
+                clearInterval(recipentsInterval);
+        }, 1);
+    });
+}
+
+for(let i=0; i<examples.length; i++) {
+    examples[i].addEventListener("click", () => {
+        let fs = 1;
+        let text = examples[i].value;
+        examples.forEach(el => el.remove());
+        let p = document.createElement("p");
+        p.setAttribute("class", "generatedText");
+        p.setAttribute("id", "examplesText");
+        p.textContent = text;
+        examplesDiv.append(p);
+        let examplesInterval = setInterval(() => {
+            p.style.fontSize = `${fs}px`;
+            fs++;
+
+            if(fs === 24)
+                clearInterval(examplesInterval);
+        }, 1);
+    });
+}
+
+for(let i=0; i<endings.length; i++) {
+    endings[i].addEventListener("click", () => {
+        let fs = 1;
+        let text = endings[i].value;
+        endings.forEach(el => el.remove());
+        let p = document.createElement("p");
+        p.setAttribute("class", "generatedText");
+        p.setAttribute("id", "endingsText");
+        p.textContent = text;
+        endingsDiv.append(p);
+        let endingsInterval = setInterval(() => {
+            p.style.fontSize = `${fs}px`;
+            fs++;
+
+            if(fs === 24)
+                clearInterval(endingsInterval);
+        }, 1);
+    });
+}
+
+
+for(let i=0; i<greetings.length; i++) {
+    greetings[i].addEventListener("click", () => {
+        let fs = 1;
+        let text = greetings[i].value;
+        greetings.forEach(el => el.remove());
+        let p = document.createElement("p");
+        p.setAttribute("class", "generatedText");
+        p.setAttribute("id", "greetingsText");
+        p.textContent = text;
+        greetingsDiv.append(p);
+        let greetingsInterval = setInterval(() => {
+            p.style.fontSize = `${fs}px`;
+            fs++;
+
+            if(fs === 24)
+                clearInterval(greetingsInterval);
+        }, 1);
+    });
+}
+
+copyBtn.addEventListener("click", () => {
+    let generatedTexts = document.querySelectorAll(".generatedText");
+    if(generatedTexts.length === 4) {
+        let toCopy = [];
+        for(let i=0; i<generatedTexts.length; i++) {
+            toCopy.push(generatedTexts[i].innerText + " ");
+        }
+        toCopy.unshift(document.querySelector(".subject__input").value);
+        toCopy.unshift(document.querySelector(".recipent__input").value)
+        toCopy[toCopy.length-1] += document.querySelector(".signature__input").value;
+        //toCopy.splice(4, 0, document.querySelector(".body__input").value);
+        toCopy[3] += `${document.querySelector(".body__input").value}`;
+        console.log(toCopy);
+        toCopy = toCopy.join('\n');
+        navigator.clipboard.writeText(toCopy);
+        console.log(toCopy);
+    }
+});
