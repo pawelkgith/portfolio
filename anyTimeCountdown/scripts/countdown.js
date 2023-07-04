25b04047dc572a8fe595c795f183cd5156ebfa50
pawelkgith <99692169+pawelkgith@users.noreply.github.com>
2023-07-04 16:55:09 +0200

    Add files via upload

--- anyTimeCountdown/scripts/countdown.js
+++ anyTimeCountdown/scripts/countdown.js
@@ -1,79 +1,95 @@
-const dateInput = document.querySelector("#dateInput"),
-secondsDiv = document.querySelector("#seconds"),
-hoursDiv = document.querySelector("#hours"),
-minutesDiv = document.querySelector("#minutes"),
-daysDiv = document.querySelector("#days"),
-startBtn = document.querySelector("#getDate"),
+const daysCap = document.querySelector("#days"),
+hoursCap = document.querySelector("#hours"),
+minutesCap = document.querySelector("#minutes"),
+secondsCap = document.querySelector("#seconds"),
 resetBtn = document.querySelector("#reset"),
+getDateBtn = document.querySelector("#getDate"),
+dateInput = document.querySelector("#dateInput"),
 expectedDateCap = document.querySelector("#expectedDate");
 
-function getCountdown() {
-    let givenDate = dateInput.value.split('/'),
-    seconds = 1000, 
-    minutes = seconds * 60, 
-    hours = minutes * 60, 
-    days = hours * 24;
-    
+var intervalsArr = [];
+
+const originalIntervalFn = window.setInterval;
+
+window.setInterval = function(fn, delay){
+  const id = originalIntervalFn(fn, delay);
+  intervalsArr.push(id);
+  return id;
+}
+
+window.clearAllInterval = function(){
+  while(intervalsArr.length) {
+    clearInterval(intervalsArr.pop());
+  }
+}
+
+resetBtn.addEventListener("click", () => {
+    window.location.reload();
+});
+
+getDateBtn.addEventListener("click", () => {
+    clearAllInterval();
+    let givenDate = dateInput.value.split('/');
+
     switch(givenDate[1]) {
-        case '1': givenDate[1] = 'Jan'; break;
-        case '2': givenDate[1] = 'Feb'; break;
-        case '3': givenDate[1] = 'Mar'; break;
-        case '4': givenDate[1] = 'Apr'; break;
-        case '5': givenDate[1] = 'May'; break;
-        case '6': givenDate[1] = 'Jun'; break;
-        case '7': givenDate[1] = 'Jul'; break;
-        case '8': givenDate[1] = 'Aug'; break;
-        case '9': givenDate[1] = 'Sep'; break;
-        case '10': givenDate[1] = 'Oct'; break;
-        case '11': givenDate[1] = 'Nov'; break;
-        case '12': givenDate[1] = 'Dec'; break;
+        case '1': givenDate[1] = "Jan"; break;
+        case '2': givenDate[1] = "Feb"; break;
+        case '3': givenDate[1] = "Mar"; break;
+        case '4': givenDate[1] = "Apr"; break;
+        case '5': givenDate[1] = "May"; break;
+        case '6': givenDate[1] = "Jun"; break;
+        case '7': givenDate[1] = "Jul"; break;
+        case '8': givenDate[1] = "Aug"; break;
+        case '9': givenDate[1] = "Sep"; break;
+        case '10': givenDate[1] = "Oct"; break;
+        case '11': givenDate[1] = "Nov"; break;
+        case '12': givenDate[1] = "Dec"; break;
     }
-    
-    const expectedDate = new Date(`${givenDate[1]} ${givenDate[0]}, ${givenDate[2]} 00:00:00`);
+
+    const expectedDate = new Date(`${givenDate[1]} ${givenDate[0]}, ${givenDate[2]}, 00:00:00`);
     var isDateValid = true;
 
-    let countdownInterval = setInterval(() => {
-        const currentDate = new Date();
-        let timeLeft = expectedDate - currentDate,
-        daysLeft = Math.floor(timeLeft / days),
-        hoursLeft = Math.floor((timeLeft % days) / hours);
-        minutesLeft = Math.floor((timeLeft % hours) / minutes),
-        secondsLeft = Math.floor((timeLeft % minutes) / seconds),
-        arr = [daysLeft, hoursLeft, minutesLeft, secondsLeft];
-
-        for(let i=0; i<arr.length; i++) {
-            if(isNaN(arr[i])) {
+    setInterval(() => {    
+        let currentDate = new Date(),
+        timeLeft = expectedDate - currentDate,
+        daysLeft = Math.floor(timeLeft / 86400000),
+        hoursLeft = Math.floor((timeLeft % 86400000) / 3600000),
+        minutesLeft = Math.floor((timeLeft % 3600000) / 60000),
+        secondsLeft = Math.floor((timeLeft % 60000) / 1000),
+        dateArr = [daysLeft, hoursLeft, minutesLeft, secondsLeft];
+        
+        for(let i=0; i<dateArr.length; i++) {
+            if(isNaN(dateArr[i])) {
                 isDateValid = false;
             }
         }
 
         if(isDateValid) {
             if(currentDate > expectedDate) {
-                arr = arr.map(el => Math.abs(el));
+                dateArr = dateArr.map(el => Math.abs(el));
                 expectedDateCap.innerText = "Given date is backwards";
             }
-           
+
             else
                 expectedDateCap.innerText = "";
 
-            for(let i=0; i<arr.length; i++) {
-                if(arr[i] < 10) {
-                    arr[i] = `0${arr[i]}`;
+            for(let i=0; i<dateArr.length; i++) {
+                if(Number(dateArr[i]) < 10) {
+                    dateArr[i] = `0${dateArr[i]}`;
                 }
             }
-            
-            daysDiv.innerText = arr[0];
-            hoursDiv.innerText = arr[1];
-            minutesDiv.innerText = arr[2];
-            secondsDiv.innerText = arr[3];
+
+            daysCap.innerText = dateArr[0];
+            hoursCap.innerText = dateArr[1];
+            minutesCap.innerText = dateArr[2];
+            secondsCap.innerText = dateArr[3];
         }
-
-        else
-            expectedDateCap.innerText = "Invalid date";
-
         
-    });
-}
+        else
+            expectedDateCap.innerText = "Invalid Date";
+        
+    }, 1000);
 
-startBtn.addEventListener("click", getCountdown);
-resetBtn.addEventListener("click", () => location.reload());
+    console.log(givenDate);
+    console.log(expectedDate);
+});
